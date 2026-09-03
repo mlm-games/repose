d5633f7d2b112c2ede913b6481cb6ef75cbefee3
mlm-games <125530737+mlm-games@users.noreply.github.com>
2026-09-03 10:09:07 +0530

    chore: wasm err fix

--- crates/repose-render-wgpu/src/callback.rs
+++ crates/repose-render-wgpu/src/callback.rs
@@ -38,37 +38,19 @@ use std::sync::Arc;
 use repose_core::{PaintCallbackInfo, PaintCallbackPayload, Rect};
 
 /// Type-map for callback-shared wgpu resources (pipelines, buffers, etc.).
-#[cfg(not(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-)))]
+#[cfg(not(target_arch = "wasm32"))]
 type AnyBox = Box<dyn std::any::Any + Send + Sync>;
-#[cfg(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-))]
+#[cfg(target_arch = "wasm32")]
 type AnyBox = Box<dyn std::any::Any>;
 
-#[cfg(not(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-)))]
+#[cfg(not(target_arch = "wasm32"))]
 pub trait MaybeSendSync: Send + Sync + 'static {}
-#[cfg(not(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-)))]
+#[cfg(not(target_arch = "wasm32"))]
 impl<T: Send + Sync + 'static> MaybeSendSync for T {}
 
-#[cfg(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-))]
+#[cfg(target_arch = "wasm32")]
 pub trait MaybeSendSync: 'static {}
-#[cfg(all(
-    target_arch = "wasm32",
-    not(feature = "fragile-send-sync-non-atomic-wasm")
-))]
+#[cfg(target_arch = "wasm32")]
 impl<T: 'static> MaybeSendSync for T {}
 
 #[derive(Default)]
